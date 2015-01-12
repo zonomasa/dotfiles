e700f6c9bef8b050305834fac019f7fb8cc13988
zonomasa <tatezono@gmail.com>
2015-01-12 23:27:53 +0900

    update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -149,8 +149,9 @@
     auto-install
     auto-complete
     anything
-;    gtags
     helm
+    multi-term
+    undo-tree
     ))
 
 (require 'cl)
@@ -351,22 +352,6 @@
 ;; migemoがrequireできる環境ならmigemoを使う
 ;;(when (require 'migemo nil t) ;第三引数がnon-nilだとloadできなかった場合にエラーではなくnilを返す
 ;;  (setq moccur-use-migemo t))
-
-;;; anything-c-moccurの設定
-(require 'anything-c-moccur)
-;; カスタマイズ可能変数の設定(M-x customize-group anything-c-moccur でも設定可能)
-(setq anything-c-moccur-anything-idle-delay 0.2 ;`anything-idle-delay'
-      anything-c-moccur-higligt-info-line-flag t ; `anything-c-moccur-dmoccur'などのコマンドでバッファの情報をハイライトする
-      anything-c-moccur-enable-auto-look-flag t ; 現在選択中の候補の位置を他のwindowに表示する
-      anything-c-moccur-enable-initial-pattern t) ; `anything-c-moccur-occur-by-moccur'の起動時にポイントの位置の単語を初期パターンにする
-
-;;; キーバインドの割当(好みに合わせて設定してください)
-(global-set-key (kbd "M-o") 'anything-c-moccur-occur-by-moccur) ;バッファ内検索
-(global-set-key (kbd "C-M-o") 'anything-c-moccur-dmoccur) ;ディレクトリ
-(add-hook 'dired-mode-hook ;dired
-          '(lambda ()
-             (local-set-key (kbd "O") 'anything-c-moccur-dired-do-moccur-by-moccur)))
-
 
 
 ;;--------------------------------------------------------------------------------
